e39038938ac974f9c07f27829262ddb064ea5c28
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-05-10 22:46:06 +0900

    Design : 버튼 클릭시 코인 보유량 반짝임

--- front/src/components/mining/MiningForm.js
+++ front/src/components/mining/MiningForm.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import * as Api from "../../api";
+import { Button, Container, Row, Col, Card, Image } from 'react-bootstrap';
 import mining_1 from '../../images/mining_1.png';
 import mining_2 from '../../images/mining_2.png';
+import '../../styles/MiningForm.css';
 
 function MiningForm() {
   const [coin, setCoin] = useState(null);
   const [imageNumber, setImageNumber] = useState(1);
+  const [shimmer, setShimmer] = useState(false);
 
   const handleAddCoin = async () => {
     try {
@@ -14,7 +17,12 @@ function MiningForm() {
         operation: "add",
       });
       setCoin(response.data.coin);
-      setImageNumber(prevImageNumber => prevImageNumber === 1 ?  2 : 1); // 이미지 번호를 교대로 변경
+      setImageNumber((prevImageNumber) => (prevImageNumber === 1 ? 2 : 1));
+      setShimmer(true); // 반짝임 활성화
+      setTimeout(() => {
+        
+        setShimmer(false); // 반짝임 비활성화
+      }, 250);
     } catch (error) {
       console.error(error);
     }
@@ -48,15 +56,32 @@ function MiningForm() {
     return <div>Loading...</div>;
   }
 
+
   return (
-    <>
-      <button type='button' onMouseDown={handleAddCoin}>+</button>
-      <br />
-      <button type='button' onMouseDown={handleDeductCoin}>-</button>
-      <br />
-      <span>코인: {coin}</span>
-      {imageNumber === 1 ? <img src={mining_1} alt="Image 1" width='50%' /> : <img src={mining_2} alt="Image 2" width='50%' />}
-    </>
+    <Container
+      className="d-flex align-items-center justify-content-center vh-100"
+      style={{ backgroundColor: "#B89E97" }}
+    >
+      <Card style={{ width: "360px" }} className="text-center">
+        <Card.Body>
+          <Button variant="primary" onMouseDown={handleAddCoin}>
+            채굴
+          </Button>
+          <br />
+          <br />
+          <span className={`coin-text ${shimmer ? "shimmer-animation" : ""}`}>
+            코인 {coin}
+          </span>
+          <br />
+          <br />
+          {imageNumber === 1 ? (
+            <Image src={mining_1} alt="Image 1" width="50%" fluid />
+          ) : (
+            <Image src={mining_2} alt="Image 2" width="50%" fluid />
+          )}
+        </Card.Body>
+      </Card>
+    </Container>
   );
 }
 

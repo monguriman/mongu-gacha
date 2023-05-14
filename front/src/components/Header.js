2669680c397aee95df2898044b4c432e950658f3
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-05-14 19:59:06 +0900

    Feat  : 코인 관련 헤더 수정

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -9,26 +9,22 @@ function Header() {
     const location = useLocation()
     const userState = useContext(UserStateContext)
     const dispatch = useContext(DispatchContext)
-    // 코인 값 상태를 로컬 상태로 관리
-    const [coin, setCoin] = useState(userState.user.coin || 0)
-    // 전역 상태(userState.user.coin)가 변경될 때마다 로컬 상태 업데이트
+    const [coin, setCoin] = useState(0) // 초기값 0으로 설정
+
     useEffect(() => {
-        setCoin(userState.user.coin)
-    }, [userState.user.coin])
-    // 전역상태에서 user가 null이 아니라면 로그인 성공 상태임.
+        if (userState.user) {
+            setCoin(userState.user.coin)
+        }
+    }, [userState.user])
+
     const isLogin = !!userState.user
 
-    // 로그아웃 클릭 시 실행되는 함수
     const logout = () => {
-        // sessionStorage 에 저장했던 JWT 토큰을 삭제함.
         sessionStorage.removeItem('userToken')
-        // dispatch 함수를 이용해 로그아웃함.
         dispatch({ type: 'LOGOUT' })
-        // 기본 페이지로 돌아감.
         navigate('/')
     }
 
-    // portfolioOwner 정보를 가져옴
     const portfolioOwner = userState.user
 
     return (

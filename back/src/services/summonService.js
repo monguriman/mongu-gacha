403473a215ba4df658695d496034cd59f68b87a0
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-05-17 20:26:50 +0900

    Fix : 1회소환 함수로 분리

--- back/src/services/summonService.js
+++ back/src/services/summonService.js
@@ -20,36 +20,41 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min //최댓값도 포함, 최솟값도 포함
 }
 
+async function drawCard(userId) {
+    const rarity = getRarity()
+    console.log(rarity, '성 등장!')
+    const candidateCards = await TotalCard.findByRarity(rarity)
+    //후보카드 중 랜덤하게 한개 카드 선택
+    const randomIndex = getRandomIntInclusive(0, candidateCards.length - 1)
+    const selectedCard = candidateCards[randomIndex]
+
+    //유저의 collection 가져오기
+    const collection = await Collection.findById({ userId })
+    
+    const cardToAdd = {
+        cardNumber: selectedCard.totalCardNumber,
+        cardRarity: selectedCard.rarity,
+    }
+    
+    //collection내의 card 배열에 selectedCard를 추가
+    collection.card.push(cardToAdd)
+    await collection.save()
+
+    return cardToAdd
+}
 class summonService {
     //카드 1장 뽑기
     //5, 4, 3, 2성 (각각 3%(1장), 10%(3장), 47%(14장), 40%(12장)) 먼저 결정
     //레어도 내의 카드 중 한장을 랜덤하게 뽑음
     static async drawAndAddOneCard({ userId }) {
-        
+
         // 코인 차감
-        await coinService.deductCoin({ userId, amount: 30 })//뽑기 1회당 가격
-
-        const rarity = getRarity()
-        console.log(rarity, '성 등장!')
-        const candidateCards = await TotalCard.findByRarity(rarity)
-        //후보카드 중 랜덤하게 한개 카드 선택
-        const randomIndex = getRandomIntInclusive(0, candidateCards.length - 1)
-        const selectedCard = candidateCards[randomIndex]
-
-        //유저의 collection 가져오기
-        const collection = await Collection.findById({ userId })
-
-        const cardToAdd = {
-            cardNumber: selectedCard.totalCardNumber,
-            cardRarity: selectedCard.rarity,
-        }
-
-        //collection내의 card 배열에 selectedCard를 추가
-        collection.card.push(cardToAdd)
-        await collection.save()
-
-        return cardToAdd;
+        await coinService.deductCoin({ userId, amount: 30 }) //뽑기 1회당 가격
+        const cardToAdd = await drawCard(userId)
+        return cardToAdd
     }
+
+    //카드 11장 연속 뽑기
 }
 
 export { summonService }

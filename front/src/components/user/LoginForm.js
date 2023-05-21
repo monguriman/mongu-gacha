75be682b2d2a4537e8a16738a2c5c70b7f109076
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-05-21 20:36:55 +0900

    Fix : 로그인 오류시 에러 알러트 뜨도록 변경

--- front/src/components/user/LoginForm.js
+++ front/src/components/user/LoginForm.js
@@ -59,6 +59,7 @@ function LoginForm() {
       // 기본 페이지로 이동함.
       navigate('/', { replace: true })
     } catch (err) {
+      alert(err.response.data.error)
       console.log('로그인에 실패하였습니다.\n', err)
     }
   }

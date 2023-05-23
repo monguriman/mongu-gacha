4bd4b825feab8aa6e720cc173e35bff844fceb49
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-05-23 15:58:37 +0900

    Misc : 필요없는 코드 지움

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -1,64 +1,70 @@
-import { User, Comment, Collection } from '../db'; // from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import { User, Comment, Collection } from '../db' // from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 class userAuthService {
     //create
     static async addUser({ name, email, password }) {
         // 이메일 중복 확인
-        const user = await User.findByEmail({ email });
+        const user = await User.findByEmail({ email })
         if (user) {
-            const errorMessage = '이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.';
-            return { errorMessage };
+            const errorMessage =
+                '이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.'
+            return { errorMessage }
         }
 
         // 비밀번호 해쉬화
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = { name, email, password: hashedPassword };
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = { name, email, password: hashedPassword }
 
         // db에 저장
-        const createdNewUser = await User.create({ newUser });
-        createdNewUser.errorMessage = null; // 문제 없이 db 저장 완료되었으므로 에러가 없음.
+        const createdNewUser = await User.create({ newUser })
+        createdNewUser.errorMessage = null // 문제 없이 db 저장 완료되었으므로 에러가 없음.
 
         const createdId = createdNewUser._id.toString()
         // 유저의 빈 콜렉션도 생성
-        const createdCollection = await Collection.create({ userId: createdId });
+        await Collection.create({ userId: createdId })
 
-        return createdNewUser;
+        return createdNewUser
     }
     // 이메일 조회
     static async findByEmail({ email }) {
-        const user = await User.findByEmail({ email });
-        return user;
+        const user = await User.findByEmail({ email })
+        return user
     }
 
     //로그인
     static async getUser({ email, password }) {
         // 이메일 db에 존재 여부 확인
-        const user = await User.findByEmail({ email });
+        const user = await User.findByEmail({ email })
 
         if (!user) {
-            const errorMessage = '해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.';
-            return { errorMessage };
+            const errorMessage =
+                '해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.'
+            return { errorMessage }
         }
 
         // 비밀번호 일치 여부 확인
-        const correctPasswordHash = user.password;
-        const isPasswordCorrect = await bcrypt.compare(password, correctPasswordHash);
+        const correctPasswordHash = user.password
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            correctPasswordHash
+        )
         if (!isPasswordCorrect) {
-            const errorMessage = '비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.';
-            return { errorMessage };
+            const errorMessage =
+                '비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.'
+            return { errorMessage }
         }
 
         // 로그인 성공 -> JWT 웹 토큰 생성
-        const secretKey = process.env.JWT_SECRET_KEY || 'jwt-secret-key';
-        const token = jwt.sign({ userId: user._id }, secretKey);
+        const secretKey = process.env.JWT_SECRET_KEY || 'jwt-secret-key'
+        const token = jwt.sign({ userId: user._id }, secretKey)
 
         // 반환할 loginuser 객체를 위한 변수 설정
-        const _id = user._id;
-        const name = user.name;
-        const description = user.description;
-        const coin = user.coin;
+        const _id = user._id
+        const name = user.name
+        const description = user.description
+        const coin = user.coin
 
         const loginUser = {
             token,
@@ -68,48 +74,49 @@ class userAuthService {
             description,
             coin,
             errorMessage: null,
-        };
+        }
 
-        return loginUser;
+        return loginUser
     }
 
     //모든 users read - 커뮤니티
     static async getUsers() {
-        const users = await User.findAll();
-        return users;
+        const users = await User.findAll()
+        return users
     }
 
     //update
     static async setUser({ userId, toUpdate }) {
         // 우선 해당 id 의 유저가 db에 존재하는지 여부 확인
-        let user = await User.findById({ userId });
+        let user = await User.findById({ userId })
         // db에서 찾지 못한 경우, 에러 메시지 반환
         if (!user) {
-            const errorMessage = '가입 내역이 없습니다. 다시 한 번 확인해 주세요.';
-            return { errorMessage };
+            const errorMessage =
+                '가입 내역이 없습니다. 다시 한 번 확인해 주세요.'
+            return { errorMessage }
         }
         // toUpdate -> name, email, description, gitLink, userImage
         for (const [fieldToUpdate, newValue] of Object.entries(toUpdate)) {
-            user = await User.update({ userId, fieldToUpdate, newValue });
-            await Comment.updateUser({ userId, fieldToUpdate, newValue });
+            user = await User.update({ userId, fieldToUpdate, newValue })
+            await Comment.updateUser({ userId, fieldToUpdate, newValue })
         }
 
-        return user;
+        return user
     }
 
     // 특정 user read
     static async getUserInfo({ userId }) {
-        const user = await User.findById({ userId });
+        const user = await User.findById({ userId })
 
         // db에서 찾지 못한 경우, 에러 메시지 반환
         if (!user) {
-            const errorMessage = '해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.';
-            return { errorMessage };
+            const errorMessage =
+                '해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.'
+            return { errorMessage }
         }
 
-        return user;
+        return user
     }
-
 }
 
-export { userAuthService };
+export { userAuthService }
